bd7a564c0374b5554c958f12dd13727051c503ae
hira480 <hiramannan8@gmail.com>
2022-03-01 01:02:58 +0600

    display search result

--- app.js
+++ app.js
@@ -5,5 +5,23 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${data.phone_name}</h4>
+                    <h5>${data.brand}</h5>
+                    <button type="button" class="btn btn-outline-success">View Details</button>
+                </div>
+         </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
