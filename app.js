6b7c0fe6ed80f96b26752bcb8ad9824ef8240994
hira480 <hiramannan8@gmail.com>
2022-02-28 23:07:03 +0600

    phone search api added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Mano spart phone is connected');
+const searchPhone = () => {
+    const searchFeald = document.getElementById('search-field');
+    const searchText = searchFeald.value;
+    searchFeald.value = '';
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.data));
+}
