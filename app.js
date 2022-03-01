5842e3a3194d59b808eb46c823d7b4be50c53df0
hira480 <hiramannan8@gmail.com>
2022-03-01 12:06:24 +0600

    load phone detail from api

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const searchPhone = () => {
 const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(data => {
+        // console.log(data);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -18,10 +19,37 @@ const displaySearchResult = data => {
                 <div class="card-body">
                     <h4 class="card-title">${data.phone_name}</h4>
                     <h5>${data.brand}</h5>
-                    <button type="button" class="btn btn-outline-success">View Details</button>
+                    <button onclick="loadPhoneDetail('${data.slug}')" type="button" class="btn btn-outline-success">View Details</button>
                 </div>
          </div>
         `;
         searchResult.appendChild(div);
     })
+}
+const loadPhoneDetail = slug => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+}
+const displayPhoneDetail = phone => {
+    console.log(phone);
+    const phoneDetail = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title">Phone Name : ${phone.name}</h4>
+                        <h5>Release Date : ${phone.releaseDate}</h5>
+                    </div>
+                </div>
+            </div>
+    `;
+    phoneDetail.appendChild(div);
 }
