ee52dd694060a8b9fcbb168c97954d0c7c1d91f8
hira480 <hiramannan8@gmail.com>
2022-03-01 19:51:41 +0600

    phone detail with others info

--- app.js
+++ app.js
@@ -25,13 +25,16 @@ const displaySearchResult = data => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="d-flex align-items-center justify-content-center">
+            <img src="${data.image}" class="card-img-top w-75" alt="...">
+            </div>
+            
                 <div class="card-body">
                     <h4 class="card-title">${data.phone_name}</h4>
                     <h5>${data.brand}</h5>
                     <button onclick="loadPhoneDetail('${data.slug}')" type="button" class="btn btn-outline-success">View Details</button>
                 </div>
-         </div>
+        </div>
         `;
         searchResult.appendChild(div);
     })
@@ -51,13 +54,24 @@ const displayPhoneDetail = phone => {
     div.classList.add('card');
     div.innerHTML = `
     <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
+                <div class="col-md-4 d-flex align-items-center justify-content-center">
+                    <img src="${phone.image}" class="img-fluid w-100 rounded-start" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
                         <h4 class="card-title">Phone Name : ${phone.name}</h4>
-                        <h5>Release Date : ${phone.releaseDate}</h5>
+                        <h5>Brand : ${phone.brand}</h5>
+                        <h6>Chipset Info : ${phone.mainFeatures.chipSet}</h6>
+                        <h6>Storage : ${phone.mainFeatures.storage}</h6>
+                        <h6>Sensors Info : ${phone.mainFeatures.sensors}</h6>
+                        <h6>Display Size : ${phone.mainFeatures.displaySize}</h6>
+                        <h6>Release Date : ${phone.releaseDate}</h6>
+                        <h5>Others Info</h5>
+                        <h6>Bluetooth : ("${phone.others?.Bluetooth}")</h6>
+                        <h6>Radio : ("${phone.others?.Radio}")</h6>
+                        <h6>GPS : ("${phone.others?.GPS}")</h6>
+                        <h6>WLAN : ("${phone.others?.WLAN}")</h6>
+                        <h6>USB : ("${phone.others?.USB}")</h6> 
                     </div>
                 </div>
             </div>
